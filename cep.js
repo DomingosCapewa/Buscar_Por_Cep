0740c020367b11914edb7eb91c914dfaf3c71136
Domingos Capewa <domingoscapewa08@gmail.com>
2025-08-13 01:29:44 -0300

    Refatorada a funcionalidade de pesquisa do CEP para usar async/await para melhor legibilidade e manutenção

--- cep.js
+++ cep.js
@@ -1,5 +1,13 @@
-let cep = document.getElementById("cep");
 
-fetch("https://viacep.com.br/ws/${cep}/json/")
-                                              .then(res => res.json())
-                                              
+      async function buscar() {
+        var cep = document.getElementById("cep").value;
+
+        var url = `https://viacep.com.br/ws/${cep}/json/`;
+        
+        var resultado = await fetch(url);
+        var json = await resultado.json();
+
+        document.getElementById("logradouro").textContent = json.logradouro || "";
+        document.getElementById("bairro").textContent = json.bairro || "";
+        document.getElementById("cidade").textContent = json.localidade || "";
+      }
